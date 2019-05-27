229c5f989ea2bce0e450d3a56683ca622fc14a07
Thazar <thazarius@gmail.com>
2019-05-27 01:26:41 +0100

    added kraj component

--- src/app/pages/truck/mapa/search/search.component.ts
+++ src/app/pages/truck/mapa/search/search.component.ts
@@ -1,18 +1,57 @@
-import { Component, ElementRef, EventEmitter, NgZone, OnInit, Output, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
-
+export interface State {
+  flag: string;
+  name: string;
+}
 
 @Component({
   selector: 'ngx-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  stateCtrl = new FormControl();
+  filteredStates: Observable<State[]>;
 
+  states: State[] = [
+    {
+      name: 'Polska',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Poland.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/1/12/Flag_of_Poland.svg'
+    },
+    {
+      name: 'Niemcy',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Germany.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg'
+    },
+    {
+      name: 'Francja',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_France.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Flag_of_France.svg'
+    },
+    {
+      name: 'Włochy',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Italy.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg'
+    }
+  ];
+  value = '';
+  adresValue= '';
   constructor() {
+    this.filteredStates = this.stateCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(state => state ? this._filterStates(state) : this.states.slice())
+      );
+  }
+  private _filterStates(value: string): State[] {
+    const filterValue = value.toLowerCase();
+
+    return this.states.filter(state => state.name.toLowerCase().indexOf(filterValue) === 0);
   }
 
-  ngOnInit() {
-    
-  }
 }
--- src/app/pages/truck/truck.module.ts
+++ src/app/pages/truck/truck.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
-import { MatIconModule, MatInputModule, MatButtonModule } from '@angular/material';
+import { MatIconModule, MatInputModule, MatButtonModule, MatAutocompleteModule,} from '@angular/material';
 import { MatGoogleMapsAutocompleteModule } from '@angular-material-extensions/google-maps-autocomplete';
+
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { TruckRoutingModule, routedComponents } from './truck-routing.module';
@@ -10,7 +11,7 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
     imports: [
-      MatIconModule, MatInputModule, MatButtonModule, MatGoogleMapsAutocompleteModule.forRoot() , ReactiveFormsModule,AngularFontAwesomeModule,
+      MatIconModule, MatInputModule, MatButtonModule, MatGoogleMapsAutocompleteModule.forRoot() , ReactiveFormsModule,AngularFontAwesomeModule, ReactiveFormsModule, MatAutocompleteModule,
       ThemeModule,
       AgmCoreModule.forRoot({
         apiKey: 'AIzaSyCl_eRT8a-rXEmSZzHcXYKi7zNI0XlGb-w',
